1a83d290f00065454cf05de3695081d0f7e357ce
Arjun Saili <arjun7.saili@gmail.com>
2022-04-04 11:49:52 -0400

    Create Dropdown Menu on click

--- src/components/Gamescreen.js
+++ src/components/Gamescreen.js
@@ -7,14 +7,23 @@ function Gamescreen(props){
 
     function createCurorOutline(e){
         setCoordinates([e.pageX, e.pageY]);
+    }
 
+    function selectCharacter(e){
+        e.stopPropagation();
+        console.log(e.target)
     }
 
     return(<div className='gamescreen' onClick={createCurorOutline}>
-        <img className="background-img" src={backgroundImage}/>
         {coordinates ? <div className="coordinate-select">
             <div className="cursor-outline" style={{top: coordinates[1], left: coordinates[0]}}></div>
+            <div className="dropdown-menu" style={{top: coordinates[1] + 15, left: coordinates[0] + 15}}>
+                <button onClick={selectCharacter} className="character-option">Neo</button>
+                <button onClick={selectCharacter} className="character-option">Patrick Star</button>
+                <button onClick={selectCharacter} className="character-option">Kratos</button>
+            </div>
         </div> : null}
+        <img className="background-img" src={backgroundImage}/>
     </div>)
 }
 
