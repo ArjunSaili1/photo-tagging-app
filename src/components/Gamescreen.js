4e26d06182bf3376a315e4a820a05d5459857a3b
Arjun Saili <arjun7.saili@gmail.com>
2022-04-05 11:57:35 -0400

    get user selected coords from cursor bounding rect

--- src/components/Gamescreen.js
+++ src/components/Gamescreen.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import backgroundImage from '../assets/background.jpg';
 import Dropdown from './Dropdown';
 import db from '../firebase'
@@ -8,7 +8,8 @@ function Gamescreen(props){
 
     const [coordinates, setCoordinates] = useState(null);
     const [unfoundCharacters, setUnfoundCharacters] = useState(["Neo", "Patrick Star", "Kratos"])
-    const locationsRef = collection(db, "locations")
+    const locationsColRef = collection(db, "locations")
+    const cursorOutlineRef = useRef(null);
 
     const dropdownStyles = coordinates ? {
         top: coordinates[1] + 15,
@@ -22,13 +23,16 @@ function Gamescreen(props){
 
     async function selectCharacter(e){
         e.stopPropagation();
-        const characterQuery = query(locationsRef, where("name", "==" , e.target.textContent))
+        const {top, bottom, left: minX , right: maxX} = cursorOutlineRef.current.getBoundingClientRect();
+        const minY = top - document.body.getBoundingClientRect().top;
+        const maxY = bottom - document.body.getBoundingClientRect().bottom
+        const characterQuery = query(locationsColRef, where("name", "==" , e.target.textContent))
         const characterQuerySnapshot = await getDocs(characterQuery);
         characterQuerySnapshot.forEach((character)=>{
-            const XDifference = character.data().coordinates[0] - coordinates[0]
-            const yDifference = character.data().coordinates[1] - coordinates[1];
-            if(-50 < XDifference && XDifference < 50 && -50 < yDifference && yDifference < 50){
-                setUnfoundCharacters(unfoundCharacters.filter(character => character !== e.target.textContent))    
+            const xCoord = character.data().coordinates[0];
+            const yCoord = character.data().coordinates[1];
+            if(xCoord > minX && xCoord < maxX && yCoord < maxY && yCoord > minY){
+                setUnfoundCharacters(unfoundCharacters.filter(unFound => unFound !== character.data().name))    
             }
             else{
                 console.log(false)
@@ -39,7 +43,7 @@ function Gamescreen(props){
 
     return(<div className='gamescreen' onClick={createCurorOutline}>
         {coordinates ? <div className="coordinate-select">
-            <div className="cursor-outline" style={{top: coordinates[1], left: coordinates[0]}}></div>
+            <div ref={cursorOutlineRef} className="cursor-outline" style={{top: coordinates[1], left: coordinates[0]}}></div>
             <Dropdown selectCharacter={selectCharacter} characters={unfoundCharacters} style={dropdownStyles}/>
         </div> : null}
         <img alt="background" className="background-img" src={backgroundImage}/>
