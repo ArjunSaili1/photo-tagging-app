5e60eac65e6b60f9a650b315461433a68518b3f4
Arjun Saili <arjun7.saili@gmail.com>
2022-04-05 08:30:44 -0400

    check db to validate selected character coordinates

--- src/components/Gamescreen.js
+++ src/components/Gamescreen.js
@@ -1,17 +1,35 @@
 import { useState } from 'react';
 import backgroundImage from '../assets/background.jpg';
+import db from '../firebase'
+import { collection, query, where, getDocs } from 'firebase/firestore'
 
 function Gamescreen(props){
 
     const [coordinates, setCoordinates] = useState(null);
+    const locationsRef = collection(db, "locations")
 
     function createCurorOutline(e){
         if(coordinates)(setCoordinates(null))
         else{setCoordinates([e.pageX, e.pageY])}
     }
 
-    function selectCharacter(e){
+    async function selectCharacter(e){
         e.stopPropagation();
+        const characterQuery = query(locationsRef, where("name", "==" , e.target.textContent))
+        const characterQuerySnapshot = await getDocs(characterQuery);
+        characterQuerySnapshot.forEach((character)=>{
+            const XDifference = character.data().coordinates[0] - coordinates[0]
+            const yDifference = character.data().coordinates[1] - coordinates[1];
+            if(-50 < XDifference && 
+                XDifference < 50 && 
+                -50 < yDifference && 
+                yDifference < 50){
+                console.log(true)
+            }
+            else{
+                console.log(false)
+            }
+        })
     }
 
     return(<div className='gamescreen' onClick={createCurorOutline}>
