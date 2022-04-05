e03a0315335bd387f705c4832910cb07868edc4b
Arjun Saili <arjun7.saili@gmail.com>
2022-04-05 09:23:49 -0400

    remove character from dropdown when found

--- src/components/Gamescreen.js
+++ src/components/Gamescreen.js
@@ -6,6 +6,7 @@ import { collection, query, where, getDocs } from 'firebase/firestore'
 function Gamescreen(props){
 
     const [coordinates, setCoordinates] = useState(null);
+    const [unfoundCharacters, setUnfoundCharacters] = useState(["Neo", "Patrick Star", "Kratos"])
     const locationsRef = collection(db, "locations")
 
     function createCurorOutline(e){
@@ -20,11 +21,8 @@ function Gamescreen(props){
         characterQuerySnapshot.forEach((character)=>{
             const XDifference = character.data().coordinates[0] - coordinates[0]
             const yDifference = character.data().coordinates[1] - coordinates[1];
-            if(-50 < XDifference && 
-                XDifference < 50 && 
-                -50 < yDifference && 
-                yDifference < 50){
-                console.log(true)
+            if(-50 < XDifference && XDifference < 50 && -50 < yDifference && yDifference < 50){
+                setUnfoundCharacters(unfoundCharacters.filter(character => character !== e.target.textContent))    
             }
             else{
                 console.log(false)
@@ -36,9 +34,9 @@ function Gamescreen(props){
         {coordinates ? <div className="coordinate-select">
             <div className="cursor-outline" style={{top: coordinates[1], left: coordinates[0]}}></div>
             <div className="dropdown-menu" style={{top: coordinates[1] + 15, left: coordinates[0] + 15}}>
-                <button onClick={selectCharacter} className="character-option">Neo</button>
-                <button onClick={selectCharacter} className="character-option">Patrick Star</button>
-                <button onClick={selectCharacter} className="character-option">Kratos</button>
+                {unfoundCharacters.map((character)=>{
+                    return <button key={character} onClick={selectCharacter} className="character-option">{character}</button>
+                })}
             </div>
         </div> : null}
         <img alt="background" className="background-img" src={backgroundImage}/>
