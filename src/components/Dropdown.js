73e264190d3e09a0844b00c75f692e56f9cf9f81
Arjun Saili <arjun7.saili@gmail.com>
2022-04-08 14:36:23 -0400

    Show character image in dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,8 +1,13 @@
 function Dropdown({selectCharacter, characters, style}){
     return(
         <div className="dropdown-menu" style={style}>
-            {characters.map((character)=>{
-                return <button key={character} onClick={selectCharacter} className="character-option">{character}</button>
+            {characters.map(({name, img})=>{
+                return(
+                    <button key={name} onClick={selectCharacter} className="character-option">
+                        {name}
+                        <img className="dropdown-img" alt={name} src={img}></img>
+                    </button>
+                )
             })}
         </div>
     )
