08d338c4f9d06079c6493e67fe00bea04dfbae16
Arjun Saili <arjun7.saili@gmail.com>
2022-04-09 18:08:00 -0400

    Add error boundry

--- src/utils/getTime.js
+++ src/utils/getTime.js
@@ -2,7 +2,9 @@ import { getDoc } from "@firebase/firestore";
 export async function getTime(docRef){
     const snapshot = await getDoc(docRef);
     if(snapshot){
-        const seconds = snapshot.data().endTime["seconds"] - snapshot.data().startTime["seconds"]
-        return(Math.trunc(seconds/60) + ":" + seconds % 60)
+        if(snapshot.data().endTime["seconds"] && snapshot.data().startTime["seconds"]){
+            const seconds = snapshot.data().endTime["seconds"] - snapshot.data().startTime["seconds"]
+            return(Math.trunc(seconds/60) + ":" + seconds % 60)
+        }
     }
 }
