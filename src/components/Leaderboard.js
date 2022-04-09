5e487d7fb7f3742d27d5157a2d44a952b065c2dd
Arjun Saili <arjun7.saili@gmail.com>
2022-04-09 15:28:22 -0400

    Create leaderboard component

---
+++ src/components/Leaderboard.js
@@ -0,0 +1,10 @@
+function Leaderboard(){
+
+    return(<div className="modal-container">
+        <div className="modal">
+            Leaderboard
+        </div>
+    </div>)
+}
+
+export default Leaderboard
