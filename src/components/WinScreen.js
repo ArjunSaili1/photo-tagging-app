716f3a1e89dae2e57196e0147b803ef43bd36067
Arjun Saili <arjun7.saili@gmail.com>
2022-04-09 21:22:39 -0400

    Allow user to add name to doc on finish game

--- src/components/WinScreen.js
+++ src/components/WinScreen.js
@@ -1,20 +1,33 @@
+import { updateDoc } from "@firebase/firestore";
 import { useContext } from "react"
+import { UserContext } from "../context/UserContext";
 import { ShowLeaderboardContext } from '../context/ShowLeaderboardContext';
 
-function WinScreen({toggle}){
+function WinScreen({setShowWinScreen}){
 
     const {setShowLeaderboard} = useContext(ShowLeaderboardContext)
+    const {userDoc} = useContext(UserContext);
     
-    function nameAdd(){
-        toggle(false)
+    async function nameAdd(e){
+        e.preventDefault();
+        await updateDoc(userDoc, {
+            name: e.target[0].value
+        });
+        setShowWinScreen(false)
         setShowLeaderboard(true)
     }
 
     return(
         <div className="modal-container">
             <div className="modal">
-                win screen
-                <button onClick={nameAdd}>click</button>
+                <div className="win-modal">
+                    <h1>You found all the characters!</h1>
+                    <h3>Enter your name to add your score to the leaderboard</h3>
+                    <form onSubmit={nameAdd}>
+                        <input className="name-field" type="text"></input>
+                        <button className="submit-name" type="submit">Join The Cool Kids</button>
+                    </form>
+                </div>
             </div>
         </div>
     )
