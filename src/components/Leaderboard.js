5229cadaecb9b32963636da4ecdb7577029853b1
Arjun Saili <arjun7.saili@gmail.com>
2022-04-09 22:28:07 -0400

    Create array of all user times

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,8 +1,29 @@
+import db from "../utils/firebase"
+import { collection, query, orderBy, getDocs } from "@firebase/firestore"
+import { useEffect } from "react";
+
 function Leaderboard(){
+
+    useEffect(()=>{
+        getAllValidUsers()
+    }, [])
+
+    async function getAllValidUsers(){
+        let timeArray = [];
+        const usersCollectionRef = collection(db, "users");
+        const usersQuery = query(usersCollectionRef, orderBy("time"))
+        const usersQuerySnapshot = await getDocs(usersQuery)
+        usersQuerySnapshot.forEach((user)=>{
+            if(user.data()["time"]){
+                console.log(user.data()["time"])
+                timeArray.push(user.data()["time"]);
+            }
+        })
+    }
 
     return(<div className="modal-container">
         <div className="modal">
-            Leaderboard
+            <h2>Leaderboard</h2>
         </div>
     </div>)
 }
