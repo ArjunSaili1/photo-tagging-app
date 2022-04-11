570adaabf77c9c0df3c62cc16c009408fa092918
Arjun Saili <arjun7.saili@gmail.com>
2022-04-11 14:17:52 -0400

    improve formatting on user time

--- src/utils/getTime.js
+++ src/utils/getTime.js
@@ -2,9 +2,13 @@ import { getDoc } from "@firebase/firestore";
 export async function getTime(docRef){
     const snapshot = await getDoc(docRef);
     if(snapshot){
-        if(snapshot.data().endTime["seconds"] && snapshot.data().startTime["seconds"]){
-            const seconds = snapshot.data().endTime["seconds"] - snapshot.data().startTime["seconds"]
-            return(Math.trunc(seconds/60) + ":" + seconds % 60)
+        if(snapshot.data().endTime && snapshot.data().startTime){
+            const duration = snapshot.data().endTime["seconds"] - snapshot.data().startTime["seconds"]
+            const mins = Math.floor((duration % 3600) / 60);
+            const seconds = duration % 60;
+            let ret = "";
+            ret += mins + ":" + (seconds < 10 ? "0" : "") + "" + seconds;
+            return(ret)
         }
     }
 }
